92e7451aec243615228f8205f8b95ef69a73b860
Fabrício <fabriciotonettolondero@gmail.com>
2023-04-04 16:56:12 -0300

    Create aula02.cs

---
+++ anotacoesFabricio/aula02.cs
@@ -0,0 +1,74 @@
+//int num;
+
+            //Console.WriteLine("Digite um número:");
+            //num = int.Parse(Console.ReadLine());
+
+            ////bool condicao = num > 0;
+
+            ////if(condicao)//condição
+            //if (num > 0)//condição
+            //{
+            //    //Código
+            //    Console.WriteLine("O número informado é POSITIVO!");
+            //}
+            //else if (num < 0)
+            //{
+            //    Console.WriteLine("NEGATIVO!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número é Zero!");
+            //}
+
+
+            //Console.WriteLine("Informe um número:");
+            //int x = int.Parse(Console.ReadLine());
+
+            //if(x % 2 == 0)
+            //{
+            //    Console.WriteLine("O número é par!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número é ímpar!");
+            //}
+
+            //Quero saber se um número é divisivel por 2, 4 e 8
+            //Console.WriteLine("Informe um número:");
+            //int x = int.Parse(Console.ReadLine());
+
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine("O número é divisivel por 2!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número não é divisivel por 2!");
+            //}
+
+            //if (x%4 ==0)
+            //{
+            //    Console.WriteLine("O número é divisivel por 4!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número não é divisivel por 4!");
+            //}
+
+            //if (x%8==0)
+            //{
+            //    Console.WriteLine("O número é divisivel por 8!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número não é divisivel por 8!");
+            //}
+
+            bool b;
+
+            b = 5 > 5; 
+            b = 5 >= 5;
+            b = 5 < 2;
+            b = 5 <= 2;
+            b = 5 == 2;
+            b = 5 != 2;
