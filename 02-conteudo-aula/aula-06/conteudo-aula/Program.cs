17dd6585d354e6491622373ed74508bee5ae2b12
LKSFerreira <lks_ferreira@hotmail.com>
2023-05-02 17:33:08 -0300

    :sparkles: Exemplificado aula sobre vetores

--- 02-conteudo-aula/aula-06/conteudo-aula/Program.cs
+++ 02-conteudo-aula/aula-06/conteudo-aula/Program.cs
@@ -1,152 +1,81 @@
-﻿// Estruturas seletivas
+﻿// Vetores em C# (Arrays)
 
 /**
 
-Switch case
+Em C# os vetores são chamados de Arrays.
 
-- Comumente chamada de "swtich" ou "swtich statement", é uma estrutura de seleção múltipla.
+Arrays são estruturas de dados que permitem armazenar vários valores do mesmo tipo.
 
-- Estrutura de seleção múltipla
+É util quando precisamos armazenar uma lista de valores.
 
-- A estrutura de seleção múltipla switch case é utilizada quando você precisa testar uma variável para verificar se ela é igual a um valor específico.
+A sintaxe para declarar um array é:
 
-- Sintaxe:
+tipo[] nomeDoVetor;
 
-switch (variavel) {
-    case valor1:
-        // bloco de código
-        break;
-    case valor2:
-        // bloco de código
-        break;
-    case valor3:
-        // bloco de código
-        break;
-    default:
-        // bloco de código
-        break;
-}
+Exemplo:
 
-Obs.: O bloco de código do default será executado caso nenhum dos valores testados seja igual ao valor da variável.
- */
+int[] numeros;
 
-// Exemplo 03: Implemente uma calculadora simples utilizando a estrutura de seleção múltipla switch case, com os operadores "+", "-", "*" e "/".
-
-// Solução 01: Utilizando a estrutura de seleção múltipla switch case
-
-Console.WriteLine("Digite o primeiro número:");
-double primeiroNumero = Convert.ToDouble(Console.ReadLine()!);
-
-Console.WriteLine("Digite o segundo número:");
-double segundoNumero = Convert.ToDouble(Console.ReadLine()!);
-
-Console.WriteLine("Digite o operador:");
-string operador = Console.ReadLine()!;
-
-double resultado = 0;
-
-switch (operador)
-{
-    case "+":
-        resultado = primeiroNumero + segundoNumero;
-        break;
-    case "-":
-        resultado = primeiroNumero - segundoNumero;
-        break;
-    case "*":
-        resultado = primeiroNumero * segundoNumero;
-        break;
-    case "/":
-        resultado = primeiroNumero / segundoNumero;
-        break;
-    default:
-        Console.WriteLine("Operador inválido!");
-        break;
-}
-
-Console.WriteLine($"O resultado da operação é: {resultado}");
-
-/**
-
-Operador ternário -> ?
-
-- O operador ternário é um operador condicional que permite que você faça uma atribuição de valor com base em uma condição.
-
-- É umas sintaxe simplificada da estrutura de seleção simples if-else.
-
-- Sintaxe:
-
-variavel = (condicao) ? valorSeVerdadeiro : valorSeFalso;
-
- */
-
-
-// Exemplo 04: Leia um número inteiro e imprima se ele é par ou ímpar.
-// Solução 01: Utilizando operator ternário
-
-Console.WriteLine("Digite um número inteiro para testa se é Par ou Ímpar:");
-int numero = int.Parse(Console.ReadLine()!);
-
-string resultadoTexto = (numero % 2 == 0) ? "par" : "ímpar";
-
-Console.WriteLine($"O número digitado é {resultadoTexto}.");
-
-
-/**
-
-Operador switch expression
-
-- A partir da versão 8 do C#, foi adicionado o operador switch expression, que é uma sintaxe simplificada da estrutura de seleção múltipla switch case.
-
-- Sintaxe:
-
-variavel = variavel switch {
-    valor1 => instrucao1,
-    valor2 => instrucao2,
-    valor3 => instrucao3,
-    _ => valorPadrao
-};
-
-Obs.: O bloco de código do _ será executado caso nenhum dos valores testados seja igual ao valor da variável.
 */
 
-// Exemplo 05: Implemente uma calculadora simples utilizando o operador ternário, com os operadores "+", "-", "*" e "/".
+// Vetores/Arrays unidimensionais
 
-Console.WriteLine("Digite o primeiro número:");
-primeiroNumero = Convert.ToDouble(Console.ReadLine()!);
+/**
 
-Console.WriteLine("Digite o segundo número:");
-segundoNumero = Convert.ToDouble(Console.ReadLine()!);
+Um array unidimensional é uma lista de valores do mesmo tipo.
 
-Console.WriteLine("Digite o operador:");
-operador = Console.ReadLine()!;
+É uma forma de armazenar vários valores do mesmo tipo em uma única variável, atraves de um índice.
 
-double resultadoOperacao = operador switch
-{
-    "+" => primeiroNumero + segundoNumero,
-    "-" => primeiroNumero - segundoNumero,
-    "*" => primeiroNumero * segundoNumero,
-    "/" => primeiroNumero / segundoNumero,
-    _ => 0
-};
+O índice de um array começa em 0.
 
-Console.WriteLine($"O resultado da operação é: {resultadoOperacao}");
+A sintaxe para declarar um array unidimensional é:
 
-/** A expressão switch é uma nova sintaxe introduzida no C# 8.0 que permite que você use uma expressão switch como uma expressão em vez de uma instrução. Isso significa que você pode usar a expressão switch em qualquer lugar onde você possa usar uma expressão, como em uma atribuição de variável, em um retorno de método ou como um argumento de método.
+tipo[] nomeDoVetor = new tipo[tamanho];
 
-Além disso, a expressão switch suporta correspondência de padrões, o que permite que você teste se uma expressão corresponde a um padrão específico e execute diferentes ações com base no resultado da correspondência. Isso pode tornar seu código mais legível e conciso ao lidar com tipos complexos de dados.
+Exemplo:
 
-Por exemplo, aqui está um exemplo de como você pode usar a expressão switch com correspondência de padrões para verificar se uma variável é um número inteiro par ou ímpar:
+int[] numeros = new int[5];
+
+Uma vez definido o tamanho do array, ele não pode ser alterado.
 */
 
-Console.WriteLine($"Digite um número inteiro para ser testado na \"switch expression\" se é Par ou Ímpar:");
-int numeroParOuImpar = int.Parse(Console.ReadLine()!);
+// Manupulação de arrays
 
-string resultadoParOuImpar = numeroParOuImpar switch
+/**
+Existem algumas formas de se atribuir valores a um array.
+
+A primeira forma é atribuir valores diretamente na declaração do array.
+*/
+
+// Exemplo:
+int[] numeros1 = new int[5] { 1, 2, 3, 4, 5 };
+
+// A segunda forma é atribuir valores pelo índice do array.
+// Exemplo:
+int[] numeros2 = new int[5];
+numeros2[0] = 1;
+numeros2[1] = 2;
+numeros2[2] = 3;
+numeros2[3] = 4;
+numeros2[4] = 5;
+
+// A terceira forma é atribuir valores pelo índice do array, mas sem definir o tamanho do array.
+// Exemplo:
+int[] numeros3 = new int[] { 1, 2, 3, 4, 5 };
+
+// A quarta forma é atribuir valores pelo índice do array, mas sem definir o tamanho do array e sem a palavra new.
+// Exemplo:
+int[] numeros4 = { 1, 2, 3, 4, 5 };
+
+// A quinta forma é atribuir valores é pelo laço de repetição.
+// Exemplo:
+int[] numeros5 = new int[5];
+for (int i = 0; i < numeros5.Length; i++)
 {
-    int n when n % 2 == 0 => "Par",
-    int n when n % 2 != 0 => "Ímpar",
-    _ => "Não é um número inteiro"
-};
+    numeros5[i] = i + 1;
+}
 
-Console.WriteLine($"{resultadoParOuImpar}");
+
+
+
+
