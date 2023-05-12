aa9d468d5ae4d9ce8a917a8552c5cb72f09e3db3
Alexandre Zamberlan <alexz@ufn.edu.br>
2023-05-12 15:16:22 -0300

    codigo manipulação de arquivo

---
+++ anotacoesAlexandre/4-Arquivo/Persistencia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Arquivo
+{
+    internal class Persistencia
+    {
+        /// <summary>
+        /// método de classe que sabe ler o conteúdo de um arquivo, linha a linha e jogar na tela
+        /// </summary>
+        /// <param name="nomeArquivo"></param>
+        public static void lerArquivoParaTela(string nomeArquivo)
+        {
+            StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8);
+            do
+            {
+                Console.WriteLine(leitor.ReadLine());
+            } while (!leitor.EndOfStream);
+            leitor.Close();
+        }
+
+
+        public static void lerArquivoExibeNomes(string nomeArquivo)
+        {
+            StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8);
+            string[] vetorLinha; //[nome, email, dataNascimento]
+            string linha;
+            do
+            {
+                linha = leitor.ReadLine();
+                vetorLinha = linha.Split(";");
+                Console.WriteLine(vetorLinha[0]);
+            } while (!leitor.EndOfStream);
+            leitor.Close();
+        }
+    }
+}
---
+++ anotacoesAlexandre/4-Arquivo/Program.cs
@@ -0,0 +1,4 @@
+﻿using _4_Arquivo;
+
+
+Persistencia.lerArquivoExibeNomes("C:\\Users\\alexz\\academia_AtosUFN_DotNet_4\\anotacoesAlexandre\\dados.dat");
