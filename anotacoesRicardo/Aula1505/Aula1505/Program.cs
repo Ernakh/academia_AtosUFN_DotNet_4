3ee795590e9e25fb21a925aada4baee51f9685da
Ricardo Frohlich da Silva <ricardosma@gmail.com>
2023-05-15 14:44:31 -0300

    Add files via upload

---
+++ anotacoesRicardo/Aula1505/Aula1505/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula1505
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+
+            Veiculo v = new Veiculo();
+            v._potencia = 500;
+            v._modelo = "Gol";
+            v.Ligar();
+
+
+            VeiculoEletrico ve = new VeiculoEletrico();
+            ve._potencia = 100;
+            ve._modelo = "Tesla";
+            ve._autonomia = 10;
+            ve.Ligar();
+            ve.Carregar();
+
+            Motocicleta m = new Motocicleta();
+            m.Ligar();
+
+        }
+    }
+}
