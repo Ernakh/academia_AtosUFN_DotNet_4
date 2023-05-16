0da93c11f5144bf8b5ae40953eedcefb894e99a6
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2023-05-16 16:11:41 -0300

    programa 4-Arquivo, rotina pesquisar + excluir

--- anotacoesAlexandre/4-Arquivo/Program.cs
+++ anotacoesAlexandre/4-Arquivo/Program.cs
@@ -1,34 +1,51 @@
 ﻿using _4_Arquivo;
-
+Console.Clear();
 List<Pessoa> listaPessoas = new List<Pessoa>();
 
-Persistencia.popularArquivoLista("C:\\Users\\alexz\\academia_AtosUFN_DotNet_4\\anotacoesAlexandre\\dados.dat", listaPessoas);
-
-//Persistencia.exibirLista(listaPessoas);
-
+Persistencia.popularArquivoLista("dados.dat", listaPessoas);
 
 string nome;
 string dataNascimento;
 Pessoa pessoa;
-for (int i = 0; i < 3; i++)
+// for (int i = 0; i < 3; i++)
+// {
+//     Console.Write("Nome: ");
+//     nome = Console.ReadLine();
+
+//     Console.Write("Data Nascimento: ");
+//     dataNascimento = Console.ReadLine();
+
+//     pessoa = new Pessoa(nome, dataNascimento);
+
+//     if (!listaPessoas.Contains(pessoa)) //????
+//     {
+//         listaPessoas.Add( pessoa );
+//         Persistencia.atualizarPessoaArquivo(pessoa, "dados.dat");
+//     } else
+//     {
+//         Console.WriteLine("Pessoa com este email já na base de dados");
+//     }
+// }
+
+Persistencia.exibirLista(listaPessoas);
+
+Console.Write("Digite nome a ser excluido: ");
+nome = Console.ReadLine();
+
+bool removeu = false;
+foreach (var item in listaPessoas)
 {
-    Console.Write("Nome: ");
-    nome = Console.ReadLine();
-
-    Console.Write("Data Nascimento: ");
-    dataNascimento = Console.ReadLine();
-
-    pessoa = new Pessoa(nome, dataNascimento);
-
-    if (!listaPessoas.Contains(pessoa)) //????
-    {
-        listaPessoas.Add( pessoa );
-        Persistencia.atualizarPessoaArquivo(pessoa, "C:\\Users\\alexz\\academia_AtosUFN_DotNet_4\\anotacoesAlexandre\\dados.dat");
-    } else
-    {
-        Console.WriteLine("Pessoa com este email já na base de dados");
+    if (nome == item.Nome) {
+        listaPessoas.Remove(item);
+        removeu = true;
+        break;
     }
 }
+if (removeu) {
+    Persistencia.gravarListaArquivo(listaPessoas, "dados.dat");
+}
+
+
 
 //Persistir na base de dados
 //Persistencia.gravarListaArquivo(listaPessoas, "C:\\Users\\alexz\\academia_AtosUFN_DotNet_4\\anotacoesAlexandre\\dados.dat");
